81c993b50064926e900701fc99c4d06cd928aa16
ivanmataras <imataras@yandex.ru>
2018-01-20 22:34:20 +0300

    Initial commit

--- task_2/src/test/java/ru/edisoft/ElementsRemoverTest.java
+++ task_2/src/test/java/ru/edisoft/ElementsRemoverTest.java
@@ -9,18 +9,18 @@ import java.util.List;
 
 public class ElementsRemoverTest {
 
-    private List<Integer> numbersInARow = new ArrayList<Integer>(Arrays.asList(1, 2, 2, 2, 2, 3, 3, 3, 4, 5));
-    private List<Integer> RandomSequence = new ArrayList<Integer>(Arrays.asList(2, 1, 2, 3, 4, 3, 3, 2, 4, 3));
+    private List<Integer> inARowSequence = new ArrayList<Integer>(Arrays.asList(1, 2, 2, 2, 2, 3, 3, 3, 4, 5));
+    private List<Integer> randomSequence = new ArrayList<Integer>(Arrays.asList(2, 1, 2, 3, 4, 3, 3, 2, 4, 3));
 
     @Test
     public void testRemoveRepeatedInARow3OrMoreTimes() throws Exception {
-        List<Integer> result = ElementsRemover.removeRepeatedInARowSequence(numbersInARow, 3);
+        List<Integer> result = ElementsRemover.removeRepeatedInARowSequence(inARowSequence, 3);
         Assert.assertEquals(Arrays.asList(1, 4, 5), result);
     }
 
     @Test
     public void testRepeatedRandom3OrMoreTimes() throws Exception {
-        List<Integer> result = ElementsRemover.removeRepeatedRandomSequence(RandomSequence, 3);
+        List<Integer> result = ElementsRemover.removeRepeatedRandomSequence(randomSequence, 3);
         Assert.assertEquals(Arrays.asList(1, 4, 4), result);
     }
 
